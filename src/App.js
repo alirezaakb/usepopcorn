7be392767acf9c68d7010ee10a236b6db301ce06
Alireza Akbari <alireza10akbari@gmail.com>
2024-03-19 04:04:24 +0330

    cleaning up fetching

--- src/App.js
+++ src/App.js
@@ -57,12 +57,14 @@ export default function App() {
     }
 
     useEffect(() => {
+        const controller = new AbortController();
+
         async function fetchMovies() {
             try {
                 setIsLoading(true);
                 setError("");
 
-                const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`);
+                const res = await fetch(`https://www.omdbapi.com/?apikey=${KEY}&s=${query}`, {signal: controller.signal});
 
                 if (!res.ok) {
                     throw new Error("Something went wrong with fetching movies!")
@@ -70,10 +72,12 @@ export default function App() {
 
                 const data = await res.json();
                 if (data.Response === "False") throw new Error("Movie not found!");
-
                 setMovies(data.Search);
+                setError("");
             } catch (e) {
-                setError(e.message);
+                if (e.name !== 'AbortError') {
+                    setError(e.message);
+                }
             } finally {
                 setIsLoading(false);
             }
@@ -85,6 +89,11 @@ export default function App() {
             return
         }
         fetchMovies();
+
+        return function () {
+            controller.abort();
+        }
+
     }, [query]);
 
 
